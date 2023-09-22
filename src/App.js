6156434accde8956ec78335546ba4844fd2540f6
Trang Lê <lethithutrang380@gmail.com>
2023-09-22 21:34:17 +0700

    fix: disable button

--- src/App.js
+++ src/App.js
@@ -5,32 +5,23 @@ let dataLists = {};
 
 function App() {
   const [totalUsers, setTotalUsers] = useState(0);
-  // const [isFetch, setIsFetch] = useState(false);
   const maxPage = totalUsers / 20;
   const [page, setPage] = useState(undefined);
-  const [imgsLoaded, setImgsLoaded] = useState(false);
-  // let page = 1;
-  // let users = {};
 
   const fetchData = async () => {
-    console.log("fetchData");
-
     try {
       const res = await fetch(`https://dummyjson.com/users?limit=20`);
       const data = await res.json();
       setTotalUsers(data?.total);
-      // setUsersList(data?.users);
       dataLists = { 0: data?.users };
       setPage(0);
     } catch (error) {
       console.log(error);
     }
   };
-
   console.log("dataLists", dataLists);
 
   const fetchDataStore = async () => {
-    console.log("fetchDataStore");
     try {
       const res = await fetch(
         `https://dummyjson.com/users?limit=20&skip=${20 * (page + 1)}`
@@ -45,40 +36,24 @@ function App() {
   const handleClickPrev = () => {
     console.log("pageclickprev", page);
     setPage(page - 1);
-    // page = page - 1;
   };
 
   const handleClickNext = () => {
-    // page = page + 1;
-    setPage(page + 1);
     console.log("pageClickNext", page);
+    setPage(page + 1);
   };
-
-  // const loadImage = (image) => {
-  //   return new Promise((resolve, reject) => {
-  //     const loadImg = new Image();
-  //     loadImg.src = image.url;
-  //     loadImg.onload = () => {
-  //       setTimeout(() => {
-  //         resolve(image.url);
-  //       }, 2000);
-  //     };
-  //   });
-  // };
-
-  // useEffect(() => {
-  //   fetchData();
-  // }, []);
-
-  useEffect(() => {
-    if (page !== undefined) {
-      fetchDataStore();
-    }
-  }, [page]);
 
   useLayoutEffect(() => {
     fetchData();
   }, []);
+
+  useEffect(() => {
+    if (page !== undefined && Object.keys(dataLists).length !== maxPage) {
+      fetchDataStore();
+    }
+  }, [page]);
+
+  console.log("data page 0", dataLists?.[0]);
 
   return (
     <div className="app">
